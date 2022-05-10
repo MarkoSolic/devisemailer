813e0434f3f865de979961259ea52f7d4ac5c168
Ubuntu <ubuntu@ip-172-31-65-60.ec2.internal>
2022-05-10 17:31:30 +0000

    added key to devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,6 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.secret_key = '786bb9a30b04f48e2e54dc8c279e09166574a1976906806d52f4bbf9579512e9d9990d28d29d14f5e5c448b20293975387db04dbf90ea35803423a90b808cfed'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
